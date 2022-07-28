617a513a7c8d376383a92017dd59d3c44130b970
JuanAntonaccio <jmantonaccio@gmail.com>
2022-07-28 21:02:25 +0000

    Ejercicio terminado, falta completar app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,15 @@
-from utils import db_connect
-engine = db_connect()
+# Arrancando hacer el pipeline del metodo
+import matplotlib.pyplot as plt
 
-# your code here
+import numpy as np 
+import pandas as pd
+import seaborn as sns
+import pickle
+
+from sklearn.ensemble import  RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+print("Comenzando el proceso del Pipeline")
+
+df_raw = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/random-forest-project-tutorial/main/titanic_train.csv') 
